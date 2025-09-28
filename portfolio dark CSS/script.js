76b0dfa02cb6d6661c4fd69a5fe4ba24a9c92863
Rajan-4900 <rajan91480@gmail.com>
2025-09-28 17:58:13 +0530

    Deleted Multiple Folder

--- portfolio dark CSS/script.js
+++
@@ -1,6 +0,0 @@
-// Dark/Light Mode Toggle
-const modeToggle = document.getElementById('mode-toggle');
-modeToggle.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-    modeToggle.innerText = document.body.classList.contains('dark-mode') ? '🌕' : '🌙';
-});
